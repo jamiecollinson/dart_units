c6c393e10f12a167d31d138f7f41c33de67463cf
Jamie Collinson <jamiecollinson@gmail.com>
2022-03-25 12:25:15 +0000

    Improve Unit

--- lib/src/unit.dart
+++ lib/src/unit.dart
@@ -12,8 +12,13 @@ abstract class Unit<T extends Unit<T>> implements Comparable<Unit> {
   num inUnit(T other) => _value / other._value;
 
   @override
-  operator ==(other) => (other is T) ? compareTo(other) == 0 : false;
+  bool operator ==(other) => (other is T) ? compareTo(other) == 0 : false;
 
-  operator >(T other) => compareTo(other) > 0;
-  operator >=(T other) => compareTo(other) >= 0;
+  @override
+  int get hashCode => _value.hashCode;
+
+  bool operator >(T other) => compareTo(other) > 0;
+  bool operator >=(T other) => compareTo(other) >= 0;
+  bool operator <(T other) => compareTo(other) < 1;
+  bool operator <=(T other) => compareTo(other) <= 0;
 }
