6b1124a0ba7b46f9efd63ea9ff28136ba252090c
Gail529 <wairimuabi@gmail.com>
2022-07-22 14:03:31 +0300

    Add volume test

--- test/dart_units_test.dart
+++ test/dart_units_test.dart
@@ -253,4 +253,138 @@ void main() {
     expect(
         Angle.arcsecond.inUnit(Angle.microarcsecond), closeTo(1e6, testDelta));
   });
+
+  test("Volume", () {
+    expect(
+        Volume.zeptoliter.inUnit(Volume.yoctoliter), closeTo(1e3, testDelta));
+    expect(Volume.attoliter.inUnit(Volume.zeptoliter), closeTo(1e3, testDelta));
+    expect(Volume.femtoliter.inUnit(Volume.attoliter), closeTo(1e3, testDelta));
+    expect(Volume.picoliter.inUnit(Volume.femtoliter), closeTo(1e3, testDelta));
+    expect(Volume.nanoliter.inUnit(Volume.picoliter), closeTo(1e3, testDelta));
+    expect(Volume.microliter.inUnit(Volume.nanoliter), closeTo(1e3, testDelta));
+    expect(
+        Volume.milliliter.inUnit(Volume.microliter), closeTo(1e3, testDelta));
+
+    expect(Volume.liter.inUnit(Volume.milliliter), closeTo(1e3, testDelta));
+    expect(Volume.liter.inUnit(Volume.centiliter), closeTo(1e2, testDelta));
+    //expect(Volume.liter.inUnit(Volume.deciliter), closeTo(1e1, testDelta));
+    expect(Volume.liter.inUnit(Volume.liter), closeTo(1e0, testDelta));
+    expect(Volume.liter.inUnit(Volume.decaliter), closeTo(1e-1, testDelta));
+    expect(Volume.liter.inUnit(Volume.hectoliter), closeTo(1e-2, testDelta));
+    expect(Volume.liter.inUnit(Volume.kiloliter), closeTo(1e-3, testDelta));
+
+    expect(Volume.kiloliter.inUnit(Volume.megaliter), closeTo(1e-3, testDelta));
+    expect(Volume.megaliter.inUnit(Volume.gigaliter), closeTo(1e-3, testDelta));
+    expect(Volume.gigaliter.inUnit(Volume.teraliter), closeTo(1e-3, testDelta));
+    expect(Volume.teraliter.inUnit(Volume.petaliter), closeTo(1e-3, testDelta));
+    expect(Volume.petaliter.inUnit(Volume.exaliter), closeTo(1e-3, testDelta));
+    expect(Volume.exaliter.inUnit(Volume.zettaliter), closeTo(1e-3, testDelta));
+
+    expect(Volume.cubiczeptometer.inUnit(Volume.cubicyoctometer),
+        closeTo(1e9, testDelta));
+    expect(Volume.cubicattometer.inUnit(Volume.cubiczeptometer),
+        closeTo(1e9, testDelta));
+    expect(Volume.cubicfemtometer.inUnit(Volume.cubicattometer),
+        closeTo(1e9, testDelta));
+    expect(Volume.cubicpicometer.inUnit(Volume.cubicfemtometer),
+        closeTo(1e9, testDelta));
+    expect(Volume.cubicnanometer.inUnit(Volume.cubicpicometer),
+        closeTo(1e9, testDelta));
+    expect(Volume.cubicmicrometer.inUnit(Volume.cubicnanometer),
+        closeTo(1e9, testDelta));
+    expect(Volume.cubicmillimeter.inUnit(Volume.cubicmicrometer),
+        closeTo(1e9, testDelta));
+
+    expect(Volume.cubicmeter.inUnit(Volume.cubicmillimeter),
+        closeTo(1e9, testDelta));
+    expect(Volume.cubicmeter.inUnit(Volume.cubiccentimeter),
+        closeTo(1e6, testDelta));
+    expect(Volume.cubicmeter.inUnit(Volume.cubicdecimeter),
+        closeTo(1e3, testDelta));
+
+    expect(Volume.liter.inUnit(Volume.cubicmeter), closeTo(1e-3, testDelta));
+    expect(
+        Volume.cubicmeter.inUnit(Volume.cubicmeter), closeTo(1e0, testDelta));
+    expect(Volume.cubicmeter.inUnit(Volume.cubicdecameter),
+        closeTo(1e-3, testDelta));
+    expect(Volume.cubicmeter.inUnit(Volume.cubichectometer),
+        closeTo(1e-6, testDelta));
+    expect(Volume.cubicmeter.inUnit(Volume.cubickilometer),
+        closeTo(1e-9, testDelta));
+
+    expect(Volume.cubickilometer.inUnit(Volume.cubicmegameter),
+        closeTo(1e-9, testDelta));
+    expect(Volume.cubicmegameter.inUnit(Volume.cubicgigameter),
+        closeTo(1e-9, testDelta));
+    expect(Volume.cubicgigameter.inUnit(Volume.cubicterameter),
+        closeTo(1e-9, testDelta));
+    expect(Volume.cubicterameter.inUnit(Volume.cubicpetameter),
+        closeTo(1e-9, testDelta));
+    expect(Volume.cubicpetameter.inUnit(Volume.cubicexameter),
+        closeTo(1e-9, testDelta));
+    expect(Volume.cubicexameter.inUnit(Volume.cubiczettameter),
+        closeTo(1e-9, testDelta));
+    expect(Volume.cubiczettameter.inUnit(Volume.cubicyottameter),
+        closeTo(1e-9, testDelta));
+
+    expect(Volume.cubicFoot.inUnit(Volume.cubicInch), closeTo(1728, testDelta));
+    //expect(Volume.cubicFoot.inUnit(Volume.cubicFeet),closeTo(1e-9, testDelta));
+    expect(Volume.cubicFoot.inUnit(Volume.cubicYard),
+        closeTo(0.037037037037037035, testDelta));
+    expect(Volume.cubicYard.inUnit(Volume.cubicMile),
+        closeTo(1.8342646506386176e-10, testDelta));
+    expect(Volume.cubicYard.inUnit(Volume.cubicFurlong),
+        closeTo(9.391362885602761e-8, testDelta));
+
+    expect(Volume.liter.inUnit(Volume.imperialGallon),
+        closeTo(0.21996924829908776, testDelta));
+    expect(Volume.liter.inUnit(Volume.imperialQuart),
+        closeTo(0.879876993196351, testDelta));
+    expect(Volume.liter.inUnit(Volume.imperialPint),
+        closeTo(1.759753986392702, testDelta));
+    expect(Volume.liter.inUnit(Volume.imperialGill),
+        closeTo(7.039015945570808, testDelta));
+    expect(Volume.liter.inUnit(Volume.imperialCup),
+        closeTo(3.519507972785404, testDelta));
+    expect(Volume.liter.inUnit(Volume.imperialFluidOunce),
+        closeTo(35.19507972785404, testDelta));
+    expect(Volume.liter.inUnit(Volume.imperialFluidDram),
+        closeTo(281.56063782283235, testDelta));
+    expect(Volume.liter.inUnit(Volume.imperialPeck),
+        closeTo(0.10998462414954388, testDelta));
+    expect(Volume.liter.inUnit(Volume.imperialBushel),
+        closeTo(0.02749615603738597, testDelta));
+
+    expect(Volume.liter.inUnit(Volume.usLiquidGallon),
+        closeTo(0.26417205235814845, testDelta));
+    expect(Volume.liter.inUnit(Volume.usLiquidQuart),
+        closeTo(1.0566882094325938, testDelta));
+    expect(Volume.liter.inUnit(Volume.usLiquidPint),
+        closeTo(2.1133764188651876, testDelta));
+    expect(Volume.liter.inUnit(Volume.usCup),
+        closeTo(4.226752837730375, testDelta));
+    expect(Volume.liter.inUnit(Volume.usLegalCup),
+        closeTo(4.166666666666667, testDelta));
+    expect(Volume.liter.inUnit(Volume.usGill),
+        closeTo(8.45350567546075, testDelta));
+    expect(Volume.liter.inUnit(Volume.usTableSpoon),
+        closeTo(67.628045403686, testDelta));
+    expect(Volume.liter.inUnit(Volume.usTeaSpoon),
+        closeTo(202.884136211058, testDelta));
+    expect(Volume.liter.inUnit(Volume.usFluidDram),
+        closeTo(270.512181614744, testDelta));
+    expect(Volume.liter.inUnit(Volume.usFluidOunce),
+        closeTo(33.814022701843, testDelta));
+
+    expect(Volume.liter.inUnit(Volume.usDryQuart),
+        closeTo(0.9080829842688559, testDelta));
+    expect(Volume.liter.inUnit(Volume.usBushel),
+        closeTo(0.028377593258401747, testDelta));
+    expect(Volume.liter.inUnit(Volume.usPeck),
+        closeTo(0.11351037303360699, testDelta));
+    expect(Volume.liter.inUnit(Volume.usDryGallon),
+        closeTo(0.22702074606721398, testDelta));
+    expect(Volume.liter.inUnit(Volume.usDryPint),
+        closeTo(1.8161659685377118, testDelta));
+  });
 }
